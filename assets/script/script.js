a2f1d99858440d3d6178bc223c5dfcd7b5284a2e
jamesigibbs <jamesigibbs180@gmail.com>
2020-04-16 21:35:06 +0000

    added script to make download cv apper in navbar

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,1 +1,17 @@
-console.log("hello")
+let scWidth = screen.width;
+const newNav = document.createElement('li');
+const download = document.createElement('a')
+
+download.className = 'nav-link'
+download.href = '#'
+download.innerText = 'Download CV';
+
+newNav.className = "navitem";
+
+
+const navbar = document.querySelector('ul.navbar-nav');
+
+if (scWidth <= 500) {
+    navbar.append(newNav);
+    navbar.appendChild(download);
+}; 
